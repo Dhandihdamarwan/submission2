15df1ea4120c17d766d587fbdcbdd6e0bee17f61
Dhandihdamarwan <dhandihdamarwan12@gmail.com>
2023-11-24 03:01:15 +0700

    first commit

---
+++ app/Http/Controllers/Mycontroller.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Mycontroller extends Controller
+{
+    public function get(){
+        return 'get';
+    }
+    public function put(){
+        return 'put';
+    }
+    public function post(){
+        return 'post';
+    }
+    public function delete(){
+        return 'delete';
+    }
+}
